943997cb35b8409a485fd6980f26f593091f2b7a
Jesse Cai <mojacc@gmail.com>
2013-09-24 09:09:52 +1000

    Removed sudo since provisioner runs as root.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,46 +2,45 @@
 
 # Ubuntu box provision
 
-sudo apt-get -y update
+apt-get -y update
 
-#local time
-sudo ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime
+# local time
+ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime
 # ln -sf /usr/share/zoneinfo/Asia/Singapore /etc/localtime
 
-#git
-sudo apt-get -y install git-core
+# git
+apt-get -y install git-core
 
-#php-fpm
-sudo apt-get -y install php5-fpm
+# php-fpm
+apt-get -y install php5-fpm
 
-#php extensions
-sudo apt-get -y install php-pear php5-cli php5-mysql php5-imap php5-gd php5-curl php5-mcrypt
+# php extensions
+apt-get -y install php-pear php5-cli php5-mysql php5-imap php5-gd php5-curl php5-mcrypt
 
-#php-apc
-sudo apt-get -y install php-apc
+# php-apc
+apt-get -y install php-apc
 
-#nginx
-sudo apt-get -y install nginx
+# nginx
+apt-get -y install nginx
 
-#link vagrant folder to hosting folder
+# link vagrant folder to hosting folder
 rm -rf /var/www
 ln -sf /vagrant /var/www
 
-#copy over configuration file (micro instance)
-sudo cp -f /vagrant/conf/nginx.conf /etc/nginx/
-sudo cp -f /vagrant/conf/virtual.conf /etc/nginx/conf.d/
-sudo cp -f /vagrant/conf/php-fpm.conf /etc/php5/fpm/
-sudo cp -f /vagrant/conf/www.conf /etc/php5/fpm/pool.d/
+# copy over configuration file (micro instance)
+cp -f /vagrant/conf/nginx.conf /etc/nginx/
+cp -f /vagrant/conf/virtual.conf /etc/nginx/conf.d/
+cp -f /vagrant/conf/php-fpm.conf /etc/php5/fpm/
+cp -f /vagrant/conf/www.conf /etc/php5/fpm/pool.d/
 
-#install sysv-rc-conf (Ubuntu version of chkconfig)
-sudo apt-get -y install sysv-rc-conf
+# install sysv-rc-conf (Ubuntu version of chkconfig)
+apt-get -y install sysv-rc-conf
 
-#autostart php-fpm & nginx
-sudo sysv-rc-conf nginx on
-sudo sysv-rc-conf php5-fpm on
+# autostart php-fpm & nginx
+sysv-rc-conf nginx on
+sysv-rc-conf php5-fpm on
 
 # start php-fpm & nginx
-sudo service nginx start
-sudo service php5-fpm start
-sudo service php5-fpm restart
-
+service nginx start
+service php5-fpm start
+service php5-fpm restart
